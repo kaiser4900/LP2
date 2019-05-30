ddd48ef65fa381f1f763c6070b91a8c4445caa96
Unknown <galemanz@ulasalle.edu.pe>
2019-05-29 23:55:26 -0500

    UVA 10226

---
+++ UVA/10226.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <map>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	t++;
+	while(t--) {
+		map<string, int> trees;
+		string tree;
+		int n = 0;
+		while(getline(cin, tree)) {
+			if(tree.compare("") == 0)
+				break;
+			trees[tree]++;
+			n++;
+		}
+		for(auto i: trees)
+        {
+            cout << i.first << " " << fixed << setprecision(4) << (i.second*100.0)/n << endl;
+
+        }
+	}
+    return 0;
+}
