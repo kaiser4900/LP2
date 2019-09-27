8a6571ec8b326cb14672c0cd517ccded7ca608c6
Unknown <galemanz@ulasalle.edu.pe>
2019-09-27 08:25:56 -0500

    correction

--- sort and search/search_algorithms.cpp
+++ sort and search/search_algorithms.cpp
@@ -22,41 +22,39 @@ bool lineal(int *A, int n,int b)
 
 bool binary(int *A, int n, int b)
 {
-    int i=0, j=n;
+    int i=0;
 
-    while(i<j)
+    while(i<n)
     {
-        int m =(i+j)/2;
+        int m =(i+n)/2;
 
         if(A[m]==b)
             return true;
 
         if(A[m]>b)
-            j=m;
+            n=m-1;
 
         else
-            i=m;
+            i=m+1;
     }
 
     return false;
 }
 bool bin_r(int *A,int n,int x,int min_=0)
 {
-    n--;
     if(min_>n)
         return false;
     int m=(min_+n)/2;
-
     if(A[m]==x)
         return true;
     if(A[m]>x)
     {
-        n=m;
+        n=m-1;
         bin_r(A,n,x,min_);
     }
     else
     {
-        min_=m;
+        min_=m+1;
         bin_r(A,n,x,min_);
     }
 }
